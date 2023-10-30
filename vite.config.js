0af35e5b3a6d6cb327ac553e5d458f6634bf374b
Vanessa Vargas <vanessa.vargas4f@gmail.com>
2023-10-30 18:43:13 -0300

    Configured aliases

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import * as path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      'assets': path.resolve(__dirname, './src/assets'),
+      'common': path.resolve(__dirname, './src/common'),
+      'components': path.resolve(__dirname, './src/components'),
+      'pages': path.resolve(__dirname, './src/pages')
+    }
+  },  
   plugins: [react()],
 })
